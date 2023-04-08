6cda43933e255ec079301f1bbe92dd062f79c883
unknown <singhabhishekj21@gmail.com>
2023-04-08 18:27:35 +0100

    remove email section

--- main.js
+++ main.js
@@ -58,23 +58,23 @@ themeButton.addEventListener("click", () => {
 });
 
 /*****************EMAIL SECTION******************* */
-document.getElementsByTagName("button").addEventListener("click", sendEmail);
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "singhabhishekj21@gmail.com",
-    Password: "password",
-    To: "singhabhishekj21@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "This is the subject",
-    Body:
-      "Name: " +
-      document.getElementById("name").value +
-      "<br> Email: " +
-      document.getElementById("email").value +
-      "<br> Phone no: " +
-      document.getElementById("phone").value +
-      "<br> Message: " +
-      document.getElementById("message").value,
-  }).then((message) => alert("Message sent successfully."));
-}
+
+// function sendEmail() {
+//   Email.send({
+//     Host: "smtp.gmail.com",
+//     Username: "singhabhishekj21@gmail.com",
+//     Password: "",
+//     To: "singhabhishekj21@gmail.com",
+//     From: document.getElementById("email").value,
+//     Subject: "This is the subject",
+//     Body:
+//       "Name: " +
+//       document.getElementById("name").value +
+//       "<br> Email: " +
+//       document.getElementById("email").value +
+//       "<br> Phone no: " +
+//       document.getElementById("phone").value +
+//       "<br> Message: " +
+//       document.getElementById("message").value,
+//   }).then((message) => alert("Message sent successfully."));
+// }
